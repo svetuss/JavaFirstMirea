b1d3509692da2958fa9827b3078b677c0a5b7e33
svetus <555.svetlana@mail.ru>
2021-09-22 09:58:30 +0300

    task6

--- src/ru/mirea/task6/TestNameable.java
+++ src/ru/mirea/task6/TestNameable.java
@@ -9,7 +9,7 @@ public class TestNameable implements Nameable
     public static void main(String[] args)
     {
         TestNameable planet = new TestNameable();
-        System.out.println(planet.getName("Мерhhhhhhhкурий"));
+        System.out.println(planet.getName("Меркурий"));
         TestNameable dog = new TestNameable();
         System.out.println(dog.getName("Бобик"));
         TestNameable car = new TestNameable();
